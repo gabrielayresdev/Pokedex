7a26b5b3fdc171ce964bec9303be235209fe0056
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-07-05 19:07:12 -0300

    Adicionada função para formatar o nome removendo os '-'.

--- js/modules/textFormat/textFormat.js
+++ js/modules/textFormat/textFormat.js
@@ -1,4 +1,8 @@
 /* Recebe uma string 'numero' como parâmetro e retorna esse número com 3 casas, adicionando "0" antes do número. Exemplo: 4 -> 004 | 53 -> 053 */
-export function formataNumero(numero) {
+export const formataNumero = (numero) => {
   return String(numero).padStart(3, 0);
-}
+};
+
+export const formataNome = (nome) => {
+  return String(nome).replace(/\-/g, " ");
+};
