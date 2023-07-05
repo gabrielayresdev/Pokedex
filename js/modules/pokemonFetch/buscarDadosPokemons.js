f5d7b1fee7c20fe5496214abeadbb1098453f689
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-07-05 19:10:13 -0300

    Mudanças na maneira de requisitar os dados dos pokémons. Anterior: múltiplas requisições eram feitas e fila, uma de cada vez. Nova: Requisições feitas paralelamente, aumentando em até 7 vezes a velocidade com que os dados são buscados.

--- js/modules/pokemonFetch/buscarDadosPokemons.js
+++ js/modules/pokemonFetch/buscarDadosPokemons.js
@@ -2,12 +2,25 @@ import buscaDadoPokemon from "./buscaDadoPokemon.js";
 
 /* Faz múltiplas requisições dos dados dos pokémons através da função buscaDadoPokemon e retorna um array com os dados adquiridos */
 export default async function buscarDadosPokemons(pokemons) {
-  const dadosPokemons = [];
+  //Fetch realizado paralelamente.
+  let promises = [];
+  let result = [];
 
   for (const pokemon of pokemons) {
+    promises.push(buscaDadoPokemon(pokemon.name));
+  }
+  const data = await Promise.all(promises);
+  data.forEach((dados) => {
+    result.push(dados);
+  });
+
+  //Busca em fila dos dados do pokémons. Aproximadamente 5 a 7 vezes mais demorada que a busca paralela.
+  /* for (const pokemon of pokemons) {
     const dados = await buscaDadoPokemon(pokemon.name);
     dadosPokemons.push(dados);
-  }
+  } */
 
-  return dadosPokemons;
+  return result;
 }
+
+//Otimizado
