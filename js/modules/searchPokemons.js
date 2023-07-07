5b57686d56d0e9272f5a6bb0c384fe2847d3e526
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-07-07 14:46:55 -0300

    Funcionalidade de buscar os pokémons através do número.

--- js/modules/searchPokemons.js
+++ js/modules/searchPokemons.js
@@ -1,4 +1,5 @@
 import Dom from "./dom/Dom.js";
+import buscaDadoPokemon from "./pokemonFetch/buscaDadoPokemon.js";
 import buscarDadosPokemons from "./pokemonFetch/buscarDadosPokemons.js";
 import { filtrados, loadTotal, pokemonsNames } from "./variaveisGlobais.js";
 import { setFiltrados } from "./variaveisGlobais.js";
@@ -15,30 +16,42 @@ export default function initSearchPokemons() {
       searchArea.classList.add("active");
       pokemonsArea.classList.remove("active");
 
-      const regexp = new RegExp(`${this.value}`, "gi");
-
-      //retorna os pokémos que possuem this.value em qualquer parte do nome para a variável filtrados
-      setFiltrados(
-        pokemonsNames.filter(({ name }) => {
-          if (regexp.test(name)) {
-            return name;
-          }
-        })
-      );
-
       //Cria um novo objeto do tipo dom para fazer a manipulação da seção pokemons-search
       const searchDom = new Dom(".pokemons-search");
-      //retorna apenas o numero padrão de pokémons exibidos por página
-      const list = filtrados.slice(0, loadTotal);
 
-      //cria os cards de loading, deleta os cards da busca anterior, pega os dados dos pokémons, cria os cards dos pokémons e deleta os cards de loading.
-      searchDom.criaCardLoading(list.length);
-      searchDom.deletaCardsPokemon();
-      const pokemonsData = await buscarDadosPokemons(list);
-      pokemonsData.forEach((item) => {
-        searchDom.criaCard(item);
-      });
-      searchDom.deletaCardsLoading();
+      //Sequência lógia para input do nome
+      if (isNaN(Number(this.value))) {
+        const regexp = new RegExp(`${this.value}`, "gi");
+
+        //retorna os pokémos que possuem this.value em qualquer parte do nome para a variável filtrados
+        setFiltrados(
+          pokemonsNames.filter(({ name }) => {
+            if (regexp.test(name)) {
+              return name;
+            }
+          })
+        );
+
+        //retorna apenas o numero padrão de pokémons exibidos por página
+        const list = filtrados.slice(0, loadTotal);
+
+        //cria os cards de loading, deleta os cards da busca anterior, pega os dados dos pokémons, cria os cards dos pokémons e deleta os cards de loading.
+        searchDom.criaCardLoading(list.length);
+        searchDom.deletaCardsPokemon();
+        const pokemonsData = await buscarDadosPokemons(list);
+        pokemonsData.forEach((item) => {
+          searchDom.criaCard(item);
+        });
+        searchDom.deletaCardsLoading();
+      } else {
+        const numero = Number(this.value);
+        const pokemonData = await buscaDadoPokemon(numero);
+
+        searchDom.criaCardLoading(1);
+        searchDom.deletaCardsPokemon();
+        searchDom.criaCard(pokemonData);
+        searchDom.deletaCardsLoading();
+      }
     } else if (this.value.length === 0) {
       //Se houver algo sendo pesquisado, a área .pokemons será exibida e a área .pokemons-search será omitida
       pokemonsArea.classList.add("active");
